5cffbabdf8a7f4a8efd3e7d57a988e5c2a545fae
Willian <willihr10@gmail.com>
2020-07-14 12:45:18 -0300

    Compilado

---
+++ Output/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1594741499,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/figurinhalucaoselosatisfação-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/logopubgrolezin-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/basketball-bounce_fkvykj4u_nwm.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
